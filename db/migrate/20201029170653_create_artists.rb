4bb91c82f4328a8de7e602987b1769e781635d5f
tolentinoel <rckrleyn@gmail.com>
2020-10-29 16:33:02 -0700

    Done.

--- db/migrate/20201029170653_create_artists.rb
+++ db/migrate/20201029170653_create_artists.rb
@@ -4,7 +4,6 @@ class CreateArtists < ActiveRecord::Migration[5.0]
       t.string :name
       t.text :bio
 
-      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,24 +13,18 @@
 ActiveRecord::Schema.define(version: 20201029171256) do
 
   create_table "artists", force: :cascade do |t|
-    t.string   "name"
-    t.text     "bio"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
+    t.text   "bio"
   end
 
   create_table "genres", force: :cascade do |t|
-    t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string "name"
   end
 
   create_table "songs", force: :cascade do |t|
-    t.string   "name"
-    t.integer  "artist_id"
-    t.integer  "genre_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string  "name"
+    t.integer "artist_id"
+    t.integer "genre_id"
   end
 
 end
